614bfd95ce8a5603c6fd868b36044ac63bac4f85
fullstack <fullstack@test.com>
2020-03-27 11:19:46 +0100

    refactoring catalogue/views.py

--- apps/catalogue/views.py
+++ apps/catalogue/views.py
@@ -1,19 +1,8 @@
-from django import shortcuts
 from oscar.apps.catalogue.views import *
 
 
 class CatalogueView(CatalogueView):
     template_name = 'catalogue/browse.html'
-    # product_model = get_model('catalogue', 'product')
-    # products = shortcuts.get_list_or_404(product_model)
-    #
-    # for product in products:
-    #     upc = product.upc
-    #     image_url = 'http://localhost:5000/picture/{}'.format(upc)
-    #
-    #     original_image = product.images.first()
-    #     original_image.original = image_url
-    #     original_image.save()
 
 
 class ProductDetailView(ProductDetailView):
